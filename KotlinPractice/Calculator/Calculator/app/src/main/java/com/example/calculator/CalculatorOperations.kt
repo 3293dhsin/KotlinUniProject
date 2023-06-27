679db2789e03bebf8f993e222809fe7872328536
Joe Cin <75717177+3293dhsin@users.noreply.github.com>
2023-06-27 12:52:32 +1000

    Delete KotlinPractice/Calculator/Calculator directory

--- KotlinPractice/Calculator/Calculator/app/src/main/java/com/example/calculator/CalculatorOperations.kt
+++
@@ -1,8 +0,0 @@
-package com.example.calculator
-
-sealed class CalculatorOperations (val symbol: String) {
-    object Add: CalculatorOperations("+")
-    object Subtract: CalculatorOperations("-")
-    object Multiply: CalculatorOperations("x")
-    object Divide: CalculatorOperations("/")
-}
